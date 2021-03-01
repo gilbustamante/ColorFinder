112f88d00543dd388671a4a986626a615ce03932
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-03-01 15:14:35 -0800

    Add form and sample text

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
+const session = require('express-session');
+const flash   = require('connect-flash');
 const ejsMate = require('ejs-mate');
 const path    = require('path');
 const app     = express();
@@ -10,8 +16,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Session setup
+const sessionConfig = {
+  name: 'GDCFSession',
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+// Flash
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error   = req.flash('error');
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.send('test')
+  res.render('index');
 });
 
 app.listen(3000, () => {

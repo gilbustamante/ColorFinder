5fdac43d19921d54106f636364b4fad9f061d803
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-03-01 14:52:18 -0800

    Add boilerplate

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const ejsMate = require('ejs-mate');
+const path    = require('path');
 const app     = express();
+
+// App setup
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('test')

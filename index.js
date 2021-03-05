318f6398c70352025fd440d5bb5be67bb2860066
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-03-05 13:32:46 -0800

    Add PORT env variable for server listen method

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.use(fileUpload());
 const paletteRoutes = require('./routes/palette');
 app.use('/', paletteRoutes);
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('Server listening...')
 });
